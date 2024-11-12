6ef71528eb85eee9da5980d85cae17e209ae17f9
MrGoodbye <mrrgooodbyee@gmail.com>
2024-11-12 20:25:41 +0700

    12/11/2024

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -2,15 +2,59 @@ import React, { useEffect, useState, useContext } from "react";
 import { BrowserRouter as Router, Route, useHistory, Redirect } from "react-router-dom";
 
 const PrivateRoutes = (props) => {
-  const token = localStorage.getItem('jwt');
+
+  const parseJwt = (token) => {
+    try {
+      return JSON.parse(atob(token.split(".")[1]));
+    } catch (e) {
+      return null;
+    }
+  };
+
+  const checkJWTExpire = () => {
+    let result = false;
+    let getJWT = localStorage.getItem('jwt');
+    if (getJWT) {
+      let decodeJWT = parseJwt(getJWT);
+      let currentDate = new Date();
+      if (decodeJWT.exp * 1000 < currentDate.getTime()) {
+        //console.log('Token đã hết hạn hoặc không tồn tại')
+        localStorage.removeItem('jwt'); //xóa localStorage
+        localStorage.removeItem('userLogin');
+        sessionStorage.clear();
+        return result
+      }
+      else {
+        //console.log('token còn hạn');
+        result = true;
+        return result
+      }
+    }
+    else {
+      localStorage.removeItem('userLogin');
+      return result
+    }
+  }
+
+  const getToken = localStorage.getItem('jwt');
   const getUserLogin = JSON.parse(localStorage.getItem('userLogin'));
 
-  if (token && token.length > 0 && getUserLogin.isLogin === true) {
-    return (
-      <>
-        <Route path={props.path} component={props.component}></Route>
-      </>
-    );
+  if (getUserLogin && getToken) {
+    const result = checkJWTExpire();
+    if (result === false) {
+      return (
+        <>
+          <Redirect to='/login'></Redirect>
+        </>
+      )
+    }
+    else {
+      return (
+        <>
+          <Route path={props.path} component={props.component}></Route>
+        </>
+      );
+    }
   }
   else {
     return (
